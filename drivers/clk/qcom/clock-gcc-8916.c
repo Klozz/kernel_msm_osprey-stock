5c94aefa9dc0f095d8bc2ca23503bf4ca5080bb2
psndna88 <psndna88@gmail.com>
2016-06-25 20:39:00 +0530

    Add 1.8 Ghz cpu + 650 Mhz gpu oc for osprey
    Thanks to Chriszuma kernel for the overclock
    
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>
    Signed-off-by: klozz <klozz707@gmail.com>

--- drivers/clk/qcom/clock-gcc-8916.c
+++ drivers/clk/qcom/clock-gcc-8916.c
@@ -348,6 +348,10 @@ static struct pll_freq_tbl apcs_pll_freq[] = {
 	F_APCS_PLL(1248000000, 65, 0x0, 0x1, 0x0, 0x0, 0x0),
 	F_APCS_PLL(1363200000, 71, 0x0, 0x1, 0x0, 0x0, 0x0),
 	F_APCS_PLL(1401600000, 73, 0x0, 0x1, 0x0, 0x0, 0x0),
+	F_APCS_PLL(1497600000, 78, 0x0, 0x1, 0x0, 0x0, 0x0),
+	F_APCS_PLL(1593600000, 83, 0x0, 0x1, 0x0, 0x0, 0x0),
+	F_APCS_PLL(1689600000, 88, 0x0, 0x1, 0x0, 0x0, 0x0),
+	F_APCS_PLL(1785600000, 95, 0x0, 0x1, 0x0, 0x0, 0x0),
 	PLL_F_END
 };
 
@@ -551,7 +555,7 @@ static struct clk_freq_tbl ftbl_gcc_camss_vfe0_clk[] = {
 	F( 266670000,	   gpll0,   3,	  0,	0),
 	F( 320000000,	   gpll0, 2.5,	  0,	0),
 	F( 400000000,	   gpll0,   2,	  0,	0),
-	F( 465000000,	   gpll2,   2,	  0,	0),
+	F( 650000000,	   gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -564,13 +568,13 @@ static struct rcg_clk vfe0_clk_src = {
 	.c = {
 		.dbg_name = "vfe0_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP3(LOW, 160000000, NOMINAL, 320000000, HIGH,
-			465000000),
+		VDD_DIG_FMAX_MAP3(LOW,160000000, NOMINAL, 320000000, HIGH,
+			650000000),
 		CLK_INIT(vfe0_clk_src.c),
 	},
 };
 
-static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
+static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_650_clk[] = {
 	F(  19200000,	      xo,   1,	  0,	0),
 	F(  50000000,  gpll0_aux,  16,	  0,	0),
 	F(  80000000,  gpll0_aux,  10,	  0,	0),
@@ -582,36 +586,21 @@ static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_465_clk[] = {
 	F( 294912000,	   gpll1,   3,	  0,	0),
 	F( 310000000,	   gpll2,   3,	  0,	0),
 	F( 400000000,  gpll0_aux,   2,	  0,	0),
-	F( 465000000,      gpll2,   2,	  0,	0),
-	F_END
-};
-
-static struct clk_freq_tbl ftbl_gcc_oxili_gfx3d_clk[] = {
-	F(  19200000,	      xo,   1,	  0,	0),
-	F(  50000000,  gpll0_aux,  16,	  0,	0),
-	F(  80000000,  gpll0_aux,  10,	  0,	0),
-	F( 100000000,  gpll0_aux,   8,	  0,	0),
-	F( 160000000,  gpll0_aux,   5,	  0,	0),
-	F( 177780000,  gpll0_aux, 4.5,	  0,	0),
-	F( 200000000,  gpll0_aux,   4,	  0,	0),
-	F( 266670000,  gpll0_aux,   3,	  0,	0),
-	F( 294912000,	   gpll1,   3,	  0,	0),
-	F( 310000000,	   gpll2,   3,	  0,	0),
-	F( 400000000,  gpll0_aux,   2,	  0,	0),
+	F( 650000000,      gpll2,   2,	  0,	0),
 	F_END
 };
 
 static struct rcg_clk gfx3d_clk_src = {
 	.cmd_rcgr_reg = GFX3D_CMD_RCGR,
 	.set_rate = set_rate_hid,
-	.freq_tbl = ftbl_gcc_oxili_gfx3d_clk,
+	.freq_tbl = ftbl_gcc_oxili_gfx3d_650_clk,
 	.current_freq = &rcg_dummy_freq,
 	.base = &virt_bases[GCC_BASE],
 	.c = {
 		.dbg_name = "gfx3d_clk_src",
 		.ops = &clk_ops_rcg,
-		VDD_DIG_FMAX_MAP3(LOW, 200000000, NOMINAL, 310000000, HIGH,
-			400000000),
+		VDD_DIG_FMAX_MAP3(LOW, 100000000, NOMINAL, 310000000, HIGH,
+			650000000),
 		CLK_INIT(gfx3d_clk_src.c),
 	},
 };
@@ -995,7 +984,7 @@ static struct rcg_clk csi1phytimer_clk_src = {
 static struct clk_freq_tbl ftbl_gcc_camss_cpp_clk[] = {
 	F( 160000000,	   gpll0,   5,	  0,	0),
 	F( 320000000,	   gpll0, 2.5,	  0,	0),
-	F( 465000000,	   gpll2,   2,	  0,	0),
+	F( 650000000,	   gpll2,   2,	  0,	0),
 	F_END
 };
 
@@ -1009,7 +998,7 @@ static struct rcg_clk cpp_clk_src = {
 		.dbg_name = "cpp_clk_src",
 		.ops = &clk_ops_rcg,
 		VDD_DIG_FMAX_MAP3(LOW, 160000000, NOMINAL, 320000000, HIGH,
-			465000000),
+			650000000),
 		CLK_INIT(cpp_clk_src.c),
 	},
 };
@@ -2798,8 +2787,8 @@ static void gcc_gfx3d_fmax(struct platform_device *pdev)
 	pr_info("%s, Version: %d, bin: %d\n", __func__, version,
 					bin);
 
-	gfx3d_clk_src.c.fmax[VDD_DIG_HIGH] = 465000000;
-	gfx3d_clk_src.freq_tbl = ftbl_gcc_oxili_gfx3d_465_clk;
+	gfx3d_clk_src.c.fmax[VDD_DIG_HIGH] = 650000000;
+	gfx3d_clk_src.freq_tbl = ftbl_gcc_oxili_gfx3d_650_clk;
 }
 
 static int msm_gcc_probe(struct platform_device *pdev)
